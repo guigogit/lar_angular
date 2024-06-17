d29e59743f7ed55a08ad41f18be3e0b4c9f48389
rodrigo <rodrigo.oliveira@spyonsky.com.br>
2024-06-17 15:31:54 -0300

    Aula_218

---
+++ resources/js/server/main.server.mjs
@@ -0,0 +1,2 @@
+import './polyfills.server.mjs';
+import{a}from"./chunk-2ARV7DAY.mjs";import"./chunk-7OSJWYT5.mjs";import"./chunk-VVCT4QZE.mjs";export{a as default};
---
+++ routes/api.php
@@ -0,0 +1,24 @@
+<?php
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| API Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register API routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "api" middleware group. Make something great!
+|
+*/
+
+Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+    return $request->user();
+});
+
+Route::get('/','PostControlador@index');
+Route::post('/','PostControlador@store');
+Route::delete('/{id}','PostControlador@destroy');
+Route::get('/like/{id}','PostControlador@like');
